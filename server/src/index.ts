0754317a701042148197eb1fe03ab5298a72bfef
Anniel Menezes Cruz <contato@annielmenezes.com>
2024-02-12 18:01:16 -0300

    feat: add delete item mutation

--- server/src/index.ts
+++ server/src/index.ts
@@ -22,6 +22,8 @@ const typeDefs = `#graphql
   
   type Mutation {
     saveItem(item: ItemInput): Item
+    
+    deleteItem(id: Int): Boolean
   }
 `;
 
@@ -37,16 +39,22 @@ const items = [
 const resolvers= {
     Query: {
         items(_, args) {
-            console.log(args)
             return items.filter(item => item.type === args.type)
         },
     },
     Mutation: {
         saveItem(_, args) {
-            const item = args.item;
+            const {item} = args;
             item.id = Math.floor(Math.random() * 1000);
             items.push(item)
             return item
+        },
+        deleteItem(_, args) {
+            const {id} = args;
+            const item = items.find(item => item.id === id);
+            if (!item) return false
+            items.splice(items.indexOf(item), 1)
+            return true
         }
     }
 }
