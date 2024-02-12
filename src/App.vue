9f18e3d0f321448b7b23b7b16f7fb0169554c387
Anniel Menezes Cruz <contato@annielmenezes.com>
2024-02-12 14:33:16 -0300

    feat: move lists to a single component

--- src/App.vue
+++ src/App.vue
@@ -8,80 +8,20 @@
       <div class="container">
         <div class="row">
           <div class="col-md">
-            <h5>
-              Prefixos
-              <span class="badge text-bg-info">{{ prefixes.length }}</span>
-            </h5>
-            <div class="card">
-              <div class="card-body">
-                <ul class="list-group">
-                  <li
-                    v-for="item in prefixes"
-                    :key="item"
-                    class="list-group-item d-flex justify-content-between align-items-center"
-                  >
-                    <div>{{ item }}</div>
-                    <button class="btn btn-info" @click="deletePrefix(item)">
-                      <span class="fa fa-trash"></span>
-                    </button>
-                  </li>
-                </ul>
-                <div class="input-group mt-3">
-                  <input
-                    v-model="prefix"
-                    class="form-control"
-                    type="text"
-                    placeholder="Digite o prefixo"
-                    @keyup.enter="addPrefix(prefix)"
-                  />
-                  <button
-                    class="btn btn-info"
-                    type="button"
-                    @click="addPrefix(prefix)"
-                  >
-                    <span class="fa fa-plus"></span>
-                  </button>
-                </div>
-              </div>
-            </div>
+            <AppItemList
+              title="Prefixos"
+              :items="prefixes"
+              @add-item="addPrefix"
+              @delete-item="deletePrefix"
+            />
           </div>
           <div class="col-md">
-            <h5>
-              Sufixos
-              <span class="badge text-bg-info">{{ suffixes.length }}</span>
-            </h5>
-            <div class="card">
-              <div class="card-body">
-                <ul class="list-group">
-                  <li
-                    v-for="item in suffixes"
-                    :key="item"
-                    class="list-group-item d-flex justify-content-between align-items-center"
-                  >
-                    <div>{{ item }}</div>
-                    <button class="btn btn-info" @click="deleteSuffix(item)">
-                      <span class="fa fa-trash"></span>
-                    </button>
-                  </li>
-                </ul>
-                <div class="input-group mt-3">
-                  <input
-                    v-model="suffix"
-                    class="form-control"
-                    type="text"
-                    placeholder="Digite o sufixo"
-                    @keyup.enter="addSuffix(suffix)"
-                  />
-                  <button
-                    class="btn btn-info"
-                    type="button"
-                    @click="addSuffix(suffix)"
-                  >
-                    <span class="fa fa-plus"></span>
-                  </button>
-                </div>
-              </div>
-            </div>
+            <AppItemList
+              title="Sufixos"
+              :items="suffixes"
+              @add-item="addSuffix"
+              @delete-item="deleteSuffix"
+            />
           </div>
         </div>
         <br />
@@ -113,14 +53,16 @@
 <script>
 import "bootstrap/dist/css/bootstrap.min.css";
 import "font-awesome/css/font-awesome.min.css";
+import AppItemList from "@/components/AppItemList.vue";
 
 export default {
   name: "App",
+  components: {
+    AppItemList,
+  },
   data() {
     return {
-      prefix: "",
       prefixes: ["Air", "Jet", "Flight"],
-      suffix: "",
       suffixes: ["Hub", "Station", "Mart"],
     };
   },
@@ -145,14 +87,12 @@ export default {
   methods: {
     addPrefix(prefix) {
       this.prefixes.push(prefix);
-      this.prefix = "";
     },
     deletePrefix(prefix) {
       this.prefixes.splice(this.prefixes.indexOf(prefix), 1);
     },
     addSuffix(suffix) {
       this.suffixes.push(suffix);
-      this.suffix = "";
     },
     deleteSuffix(suffix) {
       this.suffixes.splice(this.suffixes.indexOf(suffix), 1);
---
+++ src/components/AppItemList.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <h5>
+      {{ title }}
+      <span class="badge text-bg-info">{{ items.length }}</span>
+    </h5>
+    <div class="card">
+      <div class="card-body">
+        <ul class="list-group">
+          <li
+            v-for="item in items"
+            :key="item"
+            class="list-group-item d-flex justify-content-between align-items-center"
+          >
+            <div>{{ item }}</div>
+            <button class="btn btn-info" @click="deleteItem(item)">
+              <span class="fa fa-trash"></span>
+            </button>
+          </li>
+        </ul>
+        <div class="input-group mt-3">
+          <input
+            v-model="item"
+            class="form-control"
+            type="text"
+            placeholder="Digite o prefixo"
+            @keyup.enter="addItem(item)"
+          />
+          <button class="btn btn-info" type="button" @click="addItem(item)">
+            <span class="fa fa-plus"></span>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppItemList",
+  props: ["title", "items"],
+  data() {
+    return {
+      item: "",
+    };
+  },
+  methods: {
+    addItem(item) {
+      this.$emit("addItem", item);
+      this.item = "";
+    },
+    deleteItem(item) {
+      this.$emit("deleteItem", item);
+    },
+  },
+};
+</script>
+
+<style scoped></style>
